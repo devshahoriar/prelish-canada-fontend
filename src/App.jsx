e7aeaf9ed516fcd033b26e521aa30fda05f91922
Shahoriar <shuvo1556@gmail.com>
2023-07-30 22:30:07 +0600

    sdsf

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import AboutUs from './pages/AboutUs'
 import Agency from './pages/Agency'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/agency/:id" element={<Agency />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
