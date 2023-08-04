299eeebe1cf78c3f9732e169b5ffc5c8177bbfda
Shahoriar <shuvo1556@gmail.com>
2023-08-04 23:34:25 +0600

    dg

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Dashboard from './pages/Dashboard'
 import AddNewPropaty from './pages/AddNewPropaty'
 import Propaty from './pages/Propaty'
 import LoginSignUp from './pages/LoginSignUp'
+import DashBoardInvoisEdit from './pages/DashBoardInvoisEdit'
+import DashBoardAddNetInvoice from './pages/DashBoardAddNetInvoice'
+import DashBoardMyProfile from './pages/DashBoardMyProfile'
 
 function App() {
   return (
@@ -16,6 +19,15 @@ function App() {
         <Route path="/agency/:id" element={<Agency />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/add" element={<AddNewPropaty />} />
+        <Route path="/dashboard/myprofile" element={<DashBoardMyProfile />} />
+        <Route
+          path="/dashboard/editinvoice"
+          element={<DashBoardInvoisEdit />}
+        />
+        <Route
+          path="/dashboard/addinvoice"
+          element={<DashBoardAddNetInvoice />}
+        />
         <Route path="/propaty" element={<Propaty />} />
         <Route path="/login" element={<LoginSignUp />} />
         <Route path="/register" element={<LoginSignUp />} />
