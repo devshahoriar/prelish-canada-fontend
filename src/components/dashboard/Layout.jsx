1fa86d2fb9dfb51238af055c4fa2b567ff75ab42
Shahoriar <shuvo1556@gmail.com>
2023-08-15 16:19:23 +0600

    fgf

--- src/components/dashboard/Layout.jsx
+++ src/components/dashboard/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
 const Layout = () => {
   return (
@@ -9,7 +9,7 @@ const Layout = () => {
             <nav className="navbar navbar-expand-xl navbar-light d-block px-0 header-sticky dashboard-nav py-0">
               <div className="sticky-area shadow-xs-1 py-3">
                 <div className="d-flex px-3 px-xl-6 w-100">
-                  <a className="navbar-brand" href="index.html">
+                  <a className="navbar-brand" to="/" href="/">
                     <img src="/images/logo.png" alt="HomeID" />
                   </a>
                   <div className="ml-auto d-flex align-items-center ">
@@ -109,15 +109,15 @@ const Layout = () => {
                       </h5>
                       <ul className="list-group list-group-no-border rounded-lg">
                         <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="dashboard.html"
+                          <Link
+                            to="/dashboard"
                             className="text-heading lh-1 sidebar-link"
                           >
                             <span className="sidebar-item-icon d-inline-block mr-3 fs-20">
                               <i className="fal fa-cog" />
                             </span>
                             <span className="sidebar-item-text">Dashboard</span>
-                          </a>
+                          </Link>
                         </li>
                       </ul>
                     </li>
@@ -127,8 +127,8 @@ const Layout = () => {
                       </h5>
                       <ul className="list-group list-group-no-border rounded-lg">
                         <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="dashboard-add-new-property.html"
+                          <Link
+                            to="/dashboard/addpropaty"
                             className="text-heading lh-1 sidebar-link"
                           >
                             <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20 fs-20">
@@ -136,95 +136,10 @@ const Layout = () => {
                                 <use xlinkHref="#icon-add-new" />
                               </svg>
                             </span>
-                            <span className="sidebar-item-text">Add new</span>
-                          </a>
-                        </li>
-                        <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="dashboard-my-properties.html"
-                            className="text-heading lh-1 sidebar-link d-flex align-items-center"
-                          >
-                            <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20">
-                              <svg className="icon icon-my-properties">
-                                <use xlinkHref="#icon-my-properties" />
-                              </svg>
-                            </span>
                             <span className="sidebar-item-text">
-                              My Properties
+                              Add new Propaty
                             </span>
-                            <span className="sidebar-item-number ml-auto text-primary fs-15 font-weight-bold">
-                              29
-                            </span>
-                          </a>
-                        </li>
-                        <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="dashboard-my-favorites.html"
-                            className="text-heading lh-1 sidebar-link d-flex align-items-center"
-                          >
-                            <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20">
-                              <svg className="icon icon-heart">
-                                <use xlinkHref="#icon-heart" />
-                              </svg>
-                            </span>
-                            <span className="sidebar-item-text">
-                              My Favorites
-                            </span>
-                            <span className="sidebar-item-number ml-auto text-primary fs-15 font-weight-bold">
-                              5
-                            </span>
-                          </a>
-                        </li>
-                        <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="dashboard-save-search.html"
-                            className="text-heading lh-1 sidebar-link d-flex align-items-center"
-                          >
-                            <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20">
-                              <svg className="icon icon-save-search">
-                                <use xlinkHref="#icon-save-search" />
-                              </svg>
-                            </span>
-                            <span className="sidebar-item-text">
-                              Save Search
-                            </span>
-                            <span className="sidebar-item-number ml-auto text-primary fs-15 font-weight-bold">
-                              5
-                            </span>
-                          </a>
-                        </li>
-                        <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="dashboard-reviews.html"
-                            className="text-heading lh-1 sidebar-link d-flex align-items-center"
-                          >
-                            <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20">
-                              <svg className="icon icon-review">
-                                <use xlinkHref="#icon-review" />
-                              </svg>
-                            </span>
-                            <span className="sidebar-item-text">Reviews</span>
-                            <span className="sidebar-item-number ml-auto text-primary fs-15 font-weight-bold">
-                              29
-                            </span>
-                          </a>
-                        </li>
-                        <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="#invoice_collapse"
-                            className="text-heading lh-1 sidebar-link d-flex align-items-center"
-                            data-toggle="collapse"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                          >
-                            <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20">
-                              <i className="fal fa-file-invoice" />
-                            </span>
-                            <span className="sidebar-item-text">Invoice</span>
-                            <span className="d-inline-block ml-auto">
-                              <i className="fal fa-angle-down" />
-                            </span>
-                          </a>
+                          </Link>
                         </li>
                       </ul>
                       <div className="collapse" id="invoice_collapse">
@@ -289,34 +204,6 @@ const Layout = () => {
                             </span>
                           </a>
                         </li>
-                        <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="dashboard-my-profiles.html"
-                            className="text-heading lh-1 sidebar-link"
-                          >
-                            <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20">
-                              <svg className="icon icon-my-profile">
-                                <use xlinkHref="#icon-my-profile" />
-                              </svg>
-                            </span>
-                            <span className="sidebar-item-text">
-                              My Profile
-                            </span>
-                          </a>
-                        </li>
-                        <li className="list-group-item px-3 px-xl-4 py-2 sidebar-item">
-                          <a
-                            href="#"
-                            className="text-heading lh-1 sidebar-link"
-                          >
-                            <span className="sidebar-item-icon d-inline-block mr-3 text-muted fs-20">
-                              <svg className="icon icon-log-out">
-                                <use xlinkHref="#icon-log-out" />
-                              </svg>
-                            </span>
-                            <span className="sidebar-item-text">Log Out</span>
-                          </a>
-                        </li>
                       </ul>
                     </li>
                   </ul>
@@ -367,12 +254,12 @@ const Layout = () => {
                         </div>
                       </a>
                       <div className="dropdown-menu dropdown-menu-right w-100">
-                        <a
+                        <Link
                           className="dropdown-item"
-                          href="dashboard-my-profiles.html"
+                          to="/dashboard-my-profiles.html"
                         >
                           My Profile
-                        </a>
+                        </Link>
                         <a className="dropdown-item" href="#">
                           Logout
                         </a>
@@ -405,7 +292,7 @@ const Layout = () => {
                 </nav>
               </div>
             </header>
-          <Outlet />
+            <Outlet />
           </div>
         </div>
       </div>
